ae0874836d92dfd49c0e159f99a73b216833f111
toqanassef <toqanassef@gmail.com>
2021-01-14 23:56:51 +0200

    second commit

--- DB.js
+++ DB.js
@@ -1,9 +1,9 @@
 var db = openDatabase('pharmacy', '1.0', 'pharmacy DB', 2 * 1024 * 1024); 
 
-// db.transaction(function (tx) { 
-//     tx.executeSql('CREATE TABLE IF NOT EXISTS USERS (username UNIQUE not null,  password NOT NULL , type ,active )'); 
-//     tx.executeSql('INSERT INTO USERS VALUES ("toqa","toqa","admin","True")'); 
-//  })
+db.transaction(function (tx) { 
+    tx.executeSql('CREATE TABLE IF NOT EXISTS USERS (username UNIQUE not null,  password NOT NULL , type ,active )'); 
+    tx.executeSql('INSERT INTO USERS VALUES ("toqa","toqa","admin","True")'); 
+ })
 
 
 
@@ -12,7 +12,7 @@ var db = openDatabase('pharmacy', '1.0', 'pharmacy DB', 2 * 1024 * 1024);
 
 db.transaction(function (tx) { 
     //tx.executeSql('DELETE FROM Item ');
-    //tx.executeSql('CREATE TABLE IF NOT EXISTS Item (name unique,  quantity not null, img )'); 
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Item (name unique,  quantity not null, img )'); 
     tx.executeSql('INSERT INTO Item VALUES ("panadol",5,"")');
     tx.executeSql('INSERT INTO Item  VALUES ("glybofen",3,"")');
     tx.executeSql('INSERT INTO Item  VALUES ("octozinc",1,"")');
@@ -21,9 +21,8 @@ db.transaction(function (tx) {
 
 
 
-/*
+
 db.transaction(function (tx) { 
     tx.executeSql('CREATE TABLE IF NOT EXISTS invoice (username ,  date , type , item_name ,quantity , FOREIGN KEY (item_name) REFERENCES Item (name) , FOREIGN KEY (username) REFERENCES USERS (username) )');    
     //tx.executeSql('DELETE FROM invoice ');
 })                                                         
-*/
